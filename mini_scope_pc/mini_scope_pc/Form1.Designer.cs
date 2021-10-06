74dbcf66514b8ae4beda82f669f8ac1a9dd7e42f
KSP4EVER <szabomihaly2000@gmail.com>
2021-10-06 11:37:54 +0200

    v1

---
+++ mini_scope_pc/mini_scope_pc/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace mini_scope_pc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(873, 554);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(957, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ mini_scope_pc/mini_scope_pc/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ZedGraph;
+
+namespace mini_scope_pc
+{
+    public partial class Form1 : Form
+    {
+        GraphPane graphPane;
+        double[] buff = new double[1024];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            graphPane = zedGraphControl1.GraphPane;
+            DrawsinCurve();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void DrawsinCurve() 
+        {
+            PointPairList pointPairs = new PointPairList();
+            Random rnd = new Random();
+
+            for (int i = 0; i < buff.Length; i++) 
+            {
+                buff[i] = rnd.NextDouble();
+                double x = i;
+                double y = buff[i];
+                PointPair pointPair = new PointPair(x, y);
+
+                pointPairs.Add(pointPair);
+            }
+
+            LineItem lineItem = graphPane.AddCurve("Sin Curve", pointPairs, Color.Red, SymbolType.None);
+            zedGraphControl1.AxisChange();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PointPairList pointPairs = new PointPairList();
+            Random rnd = new Random();
+
+            for (int i = 0; i < buff.Length; i++)
+            {
+                buff[i] = Math.Sin(i * Math.PI / 180);
+                double x = i - 1024/2;
+                double y = buff[i];
+                PointPair pointPair = new PointPair(x, y);
+
+                pointPairs.Add(pointPair);
+            }
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            LineItem lineItem = graphPane.AddCurve("Sin Curve", pointPairs, Color.Red, SymbolType.None);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.GraphPane.YAxis.Scale.Min = -1;
+            zedGraphControl1.GraphPane.YAxis.Scale.Max =  1;
+            zedGraphControl1.GraphPane.XAxis.Scale.Min = -512;
+            zedGraphControl1.GraphPane.XAxis.Scale.Max = 512;
+        }
+    }
+}
